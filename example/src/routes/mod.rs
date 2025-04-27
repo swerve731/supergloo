6f4190cd4eddc4fb0e53db59ec31688c5f54a09c
nioe <eoinmitchell39@proton.me>
2025-04-27 18:29:36 -0400

    get ready to publish on crates.io

--- example/src/routes/mod.rs
+++ example/src/routes/mod.rs
@@ -6,7 +6,7 @@ use gloo_macros::gloo_handler;
 use supergloo::GlooHandler;
 
 
-// this creates the "/" route
+// this creates the "/" route 
 #[gloo_handler("get")]
 async fn base() -> Response {
     "home".into_response()
