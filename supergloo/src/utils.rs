16f7868e482cce409caa308f6cf8ea73b27289d4
Eoin Mitchell <eoinmitchell39@proton.me>
2025-05-16 17:16:04 -0400

    stash

--- supergloo/src/utils.rs
+++ supergloo/src/utils.rs
@@ -11,14 +11,7 @@ pub fn route_path_from_mod_path(mod_path: &str) -> String {
         }
     }
 
-    for i in 0..path.len() {
-        if path[i] == "layout" {
-            path.remove(i);
-            break;
-        } else {
-            continue;
-        }
-    }
+
 
     let mut path = path.join("/").replace("::", "/");
     if !path.starts_with("/") {
@@ -31,12 +24,3 @@ pub fn route_path_from_mod_path(mod_path: &str) -> String {
 
 }
 
-// Helper function to box the future returned by an async fn
-// This is needed because the type `async fn() -> maud::Markup` is not directly storable
-// The function needs to be async to be an axum method router and is a better user experience
-// in the enum without knowing the exact future type, which is unnameable.
-// We erase the specific future type using dynamic dispatch (`dyn Future`).
-pub fn box_view_fn(f: impl std::future::Future<Output = maud::Markup> + Send + 'static)
-    -> std::pin::Pin<Box<dyn std::future::Future<Output = maud::Markup> + Send>> {
-    Box::pin(f)
-}
